998dc9739f3947f6f8a7f656a7037129a23aa563
Scala Steward <me@scala-steward.org>
2021-11-01 23:35:16 +0100

    Update testcontainers-scala-mariadb, ... to 0.39.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val mariaDbVersion = "2.7.4"
   val postgresVersion = "42.3.1"
   val slf4jVersion = "1.7.32"
-  val testContainersVersion = "0.39.10"
+  val testContainersVersion = "0.39.11"
   val tikaVersion = "2.1.0"
   val icu4jVersion = "69.1"
   val kindProjectorVersion = "0.10.3"
