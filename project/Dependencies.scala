94b26962070fe2616fb7b94059bbd94e03887787
Scala Steward <me@scala-steward.org>
2022-02-19 20:20:33 +0100

    Update fs2-core, fs2-io to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val fs2Version = "3.2.4"
+  val fs2Version = "3.2.5"
   val h2Version = "2.1.210"
   val munitVersion = "0.7.29"
   val munitCatsEffectVersion = "1.0.7"
