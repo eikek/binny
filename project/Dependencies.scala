d28bfac2a2eabc4b164d9e43d467d2d604bbf084
Scala Steward <scala_steward@virtuslab.com>
2022-09-07 15:15:57 +0000

    Update fs2-core, fs2-io to 3.2.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val fs2Version = "3.2.12"
+  val fs2Version = "3.2.14"
   val h2Version = "2.1.214"
   val munitVersion = "0.7.29"
   val munitCatsEffectVersion = "1.0.7"
