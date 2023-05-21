4eb6dbecda1a7af7c6a4f974b0775f77ef727813
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2023-05-21 06:10:05 +0000

    Update minio to 8.5.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   val tikaVersion = "2.8.0"
   val icu4jVersion = "69.1"
   val kindProjectorVersion = "0.10.3"
-  val minioVersion = "8.5.2"
+  val minioVersion = "8.5.3"
 
   val minio = Seq(
     "io.minio" % "minio" % minioVersion
