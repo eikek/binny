ea35f027380c6b58d5a0a5291892bc767c2998f9
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2024-01-21 06:09:09 +0000

    Update fs2-core, fs2-io to 3.9.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val fs2Version = "3.9.3"
+  val fs2Version = "3.9.4"
   val h2Version = "2.2.224"
   val munitVersion = "0.7.29"
   val munitCatsEffectVersion = "1.0.7"
