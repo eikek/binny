43f91e26ee24a707de69e9dd9c910e247b6acbf6
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2024-05-24 21:18:03 +0000

    Update munit, munit-scalacheck to 1.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val fs2Version = "3.9.4"
   val h2Version = "2.2.224"
-  val munitVersion = "0.7.29"
+  val munitVersion = "1.0.0"
   val munitCatsEffectVersion = "1.0.7"
   val mariaDbVersion = "3.3.3"
   val postgresVersion = "42.7.3"
