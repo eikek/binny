746a323883b746429cc1266511005889d0b47f24
Scala Steward <scala_steward@virtuslab.com>
2024-10-20 15:08:11 +0000

    Update tika-core to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val mariaDbVersion = "3.4.1"
   val postgresVersion = "42.7.4"
   val slf4jVersion = "2.0.13"
-  val tikaVersion = "2.9.2"
+  val tikaVersion = "3.0.0"
   val icu4jVersion = "69.1"
   val kindProjectorVersion = "0.10.3"
   val minioVersion = "8.5.12"
