c278772710bd624b8fe085cf25b2ba0255c02fe3
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2024-12-10 10:14:10 +0000

    Update mariadb-java-client to 3.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val h2Version = "2.3.232"
   val munitVersion = "0.7.29"
   val munitCatsEffectVersion = "1.0.7"
-  val mariaDbVersion = "3.5.0"
+  val mariaDbVersion = "3.5.1"
   val postgresVersion = "42.7.4"
   val slf4jVersion = "2.0.13"
   val tikaVersion = "2.9.2"
