1654130291ba870a1529197c89dbc1a2dcb1598b
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2025-09-25 06:25:28 +0000

    Update h2 to 2.4.240

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val fs2Version = "3.9.4"
-  val h2Version = "2.3.232"
+  val h2Version = "2.4.240"
   val munitVersion = "0.7.29"
   val munitCatsEffectVersion = "1.0.7"
   val mariaDbVersion = "3.5.6"
